33bc2c35d057f02bc197cb9449ff6a525520a747
Nadeem Khawaja <40156720+nadeemkhawaja@users.noreply.github.com>
2023-03-15 07:21:53 -0500

    Use Case 1
    This use case will create a user with tenant admin rights, it will create a security domain, two tenants and associate the tenants to the security domain. The user will have tenant-admin write privileges to the tenants.

---
+++ UseCase1/main.tf
@@ -0,0 +1,120 @@
+terraform {
+  required_providers {
+    aci = {
+      source  = "CiscoDevNet/aci"
+      #version = "0.5.2"
+    }
+  }
+  #required_version = ">= 0.13"
+}
+
+provider "aci" {
+  username = "${var.username}"
+  password =  "${var.password}"
+  url       = "${var.apic_url}"
+  insecure = true
+}
+
+resource "aci_tenant" "Tenant-1" {
+  name        = "${var.tenant-1_name}"   
+  description = "This is Network-Centric Tenant"
+}
+
+resource "aci_tenant" "Tenant-2" {
+  name        = "${var.tenant-2_name}"    
+  description = "This is Application-Centric Tenant"
+}
+
+resource "aci_local_user" "test" {
+    name                = "test"
+    account_status      = "active"
+    annotation          = "test-admin"
+    cert_attribute      = "example"
+    clear_pwd_history   = "no"
+    description         = "test-tenant-admin from terraform"
+    email               = "example@email.com"
+    expiration          = "2030-01-01 00:00:00"
+    expires             = "yes"
+    first_name          = "fname"
+    last_name           = "lname"
+    name_alias          = "alias_name"
+    otpenable           = "no"
+    otpkey              = ""
+    phone               = "1234567890"
+    pwd                 = "Vzure123"
+    pwd_life_time       = "20"
+    pwd_update_required = "no"
+    rbac_string         = "example"
+}
+
+resource "aci_user_security_domain" "test" {
+  local_user_dn  = aci_local_user.test.id
+  name  = "test"
+  annotation = "orchestrator:terraform"
+  name_alias = "test"
+  description = "from Terraform"
+}
+
+resource "aci_user_security_domain_role" "test" {
+  user_domain_dn  = aci_user_security_domain.test.id
+  annotation      = "orchestrator:terraform"
+  name            = "tenant-admin"
+  priv_type       = "writePriv"
+  name_alias      = "user_role_alias"
+  description     = "From Terraform"
+}
+
+resource "aci_user_security_domain" "all" {
+  local_user_dn  = aci_local_user.test.id
+  name  = "all"
+  annotation = "orchestrator:terraform"
+  name_alias = "all"
+  description = "from Terraform"
+}
+
+resource "aci_user_security_domain_role" "all" {
+  user_domain_dn  = aci_user_security_domain.test.id
+  annotation      = "orchestrator:terraform"
+  name            = "admin"
+  priv_type       = "readPriv"
+  name_alias      = "user_role_alias"
+  description     = "From Terraform"
+}
+
+resource "aci_rest" "security-domain-1" {
+  path       = "/api/node/mo/uni/tn-Tenant-1/domain-test.json"
+  payload = <<EOF
+{
+    "aaaDomainRef": {
+        "attributes": {
+            "dn": "uni/tn-Tenant-1/domain-test",
+            "name": "test",
+            "status": "created",
+            "rn": "domain-test"
+        },
+        "children": []
+    }
+}
+  EOF
+}
+
+resource "aci_rest" "security-domain-2" {
+  path       = "/api/node/mo/uni/tn-Tenant-2/domain-test.json"
+  payload = <<EOF
+{
+    "aaaDomainRef": {
+        "attributes": {
+            "dn": "uni/tn-Tenant-2/domain-test",
+            "name": "test",
+            "status": "created",
+            "rn": "domain-test"
+        },
+        "children": []
+    }
+}
+  EOF
+}
+
+
+
+
---
+++ UseCase1/terraform.tfvars
@@ -0,0 +1,5 @@
+username = "admin"
+password = "Vzure123"
+apic_url = "https://10.1.1.50"
+tenant-1_name = "Tenant-1"
+tenant-2_name = "Tenant-2"
